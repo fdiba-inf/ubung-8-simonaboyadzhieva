db33ae2ee62ab8b41333670e952e3578c6cabc34
simonaboyadzhie <91485562+simonaboyadzhieva@users.noreply.github.com>
2021-12-01 16:56:44 +0000

    snow21

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -1,28 +1,28 @@
 package exercise8;
-
+import java.util.Scanner;
 public class Point {
     double x;
     double y;
 
     public Point() {
-       x = 0;
-       y = 0;
+        x = 0;
+        y = 0;
     }
 
     public Point(double x, double y) {
-        this.x = x; 
-        this.y = y;
+      this.x = x;
+      this.y = y;
     }
 
     public Point(Point otherPoint) {
-        x = otherPoint.x;
-        y = otherPoint.y;
+       x = otherPoint.x;
+       y = otherPoint.y;
     }
 
     public void initialize() {
-      
-      x = Utils.INPUT.nextDouble();
-      y = Utils.INPUT.nextDouble();
+        Scanner input = new Scanner(System.in);
+        x = input.nextDouble();
+         y = input.nextDouble();
 
     }
 
@@ -34,19 +34,17 @@ public class Point {
     public Point createNewTranslatedPoint(double xDelta, double yDelta) {
         double newX = x + xDelta;
         double newY = y + yDelta;
-       Point p = new Point(newX, newY);
-       return p;
+        return new Point(newX, newY);
     }
 
     public boolean equals(Point otherPoint) {
-      boolean xEqual = Utils.equals(x, otherPoint.x);
-      boolean yEqual = Utils.equals(y, otherPoint.y);
-      return xEqual && yEqual;
-
+       boolean xEqual = Utils.equals(x, otherPoint.x);
+       boolean yEqual = Utils.equals(y, otherPoint.y);
+        return xEqual && yEqual;
     }
 
     public String toString() {
-       
-        return "(" + x + "," + y + ")";
+        
+        return "(" + x + ", " + y + ")";
     }
 }
